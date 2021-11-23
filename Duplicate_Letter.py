c058a1db20837183b479329f1939cff007478173
Ashwin Sreedhar <ashwinsreedhar4@gmail.com>
2021-11-23 22:09:23 +0530

    Success

---
+++ Duplicate_Letter.py
@@ -0,0 +1,15 @@
+a = "Ashwin"
+
+b = []
+count=0
+for i in a:
+    b.append(i)
+for j in range(0,len(b)-1):
+    for k in range(j+1,len(b)-1):
+        if(b[j]==b[k]):
+            count = count+1
+            print ("Duplicate Letter is ",b[j])
+            
+
+print(b)
+print(count)
